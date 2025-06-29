4c2553bba7f1ee3fc4c817de7cdbdf41d3ce2d8d
AliaNormality <120566037+AbnormalNormality@users.noreply.github.com>
2025-06-29 17:11:29 +1000

    Update keybinds.js

--- scripts/keybinds.js
+++ scripts/keybinds.js
@@ -10,5 +10,5 @@ export function openRepository() {
 }
 
 export const keybindListener = document.addEventListener("keyup", (event) => {
-  if (event.altKey && event.code === "KeyG") openRepository;
+  if (event.altKey && event.code === "KeyG") openRepository();
 });
